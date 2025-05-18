43d2732fbc3e7aa53ab30c46c3ab316aa22f036c
lysoll17 <166395787+lysoll17@users.noreply.github.com>
2025-05-18 14:42:46 +0200

    Delete Formularz directory

--- Formularz/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "szkola");
-if ($conn->connect_error) {
-    die("Błąd połączenia z bazą danych: " . $conn->connect_error);
-}
-?>
--- Formularz/usun.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "config.php";
-$id = $_GET['id'];
-
-$conn->query("DELETE FROM uzytkownicy WHERE id = $id");
-header("Location: index.php");
